737633da4482169c97bdd84dc5add7926a915d6c
webreflection <andrea.giammarchi@gmail.com>
2015-09-15 18:32:16 +0100

    dom-class gitstrap initialized

---
+++ README.md
@@ -0,0 +1,5 @@
+dom-class
+=========
+
+[![build status](https://secure.travis-ci.org/WebReflection/dom-class.svg)](http://travis-ci.org/WebReflection/dom-class)
+
---
+++ test/dom-class.js
@@ -0,0 +1,13 @@
+//remove:
+var main = require('../build/dom-class.node.js');
+//:remove
+
+wru.test([
+  {
+    name: "main",
+    test: function () {
+      wru.assert(typeof main == "object");
+      // wru.assert(0);
+    }
+  }
+]);
