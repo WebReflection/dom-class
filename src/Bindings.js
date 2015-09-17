a9fd727c968911e3a6fcb04a411f116dd2935277
webreflection <andrea.giammarchi@gmail.com>
2015-09-17 23:38:39 +0100

    better Bindings

--- src/Bindings.js
+++ src/Bindings.js
@@ -1,19 +1,30 @@
 var Bindings = (function () {'usew strict';
   /*! (C) 2015 Andrea Giammarchi - Mit Style License */
   var
+    create = Object.create,
     dP = Object.defineProperty,
+    gOPD = Object.getOwnPropertyDescriptor,
     ignore = /IFRAME|NOFRAMES|NOSCRIPT|SCRIPT|SELECT|STYLE|TEXTAREA|[a-z]/,
     oneWay = /\{\{\S+?\}\}/g,
     comma = /\s*,\s*/,
     colon = /\s*:\s*/,
-    noOp = function () {}
+    noOp = function () {},
+    createGetSet = function (get, set) {
+      return {
+        configurable: true,
+        enumerable: true,
+        get: get,
+        set: set
+      };
+    },
+    MO = window.MutationObserver,
+    noMO = !MO
   ;
   function boundTextNode(bindings, key, node) {
-    dP(bindings, key, {
-      enumerable: true,
-      get: function () { return node.nodeValue; },
-      set: function (value) { node.nodeValue = value; }
-    });
+    dP(bindings, key, createGetSet(
+      function get() { return node.nodeValue; },
+      function set(value) { node.nodeValue = value; }
+    ));
     return node;
   }
   function grabAllTextNodes(node, allText) {
@@ -34,16 +45,17 @@ var Bindings = (function () {'usew strict';
   }
   return {
     withBindings: function (info) {
-      this.innerHTML = info.template;
+      if (info.template) this.innerHTML = info.template;
       var
         document = this.ownerDocument || document,
+        onChanged = this.attributeChangedCallback || noOp,
         bindings = info.bindings || {},
-        values = Object.create(null),
+        values = create(null),
+        map = create(null),
         textNodes = grabAllTextNodes(this, []),
         attributes = textNodes.slice.call(
           this.querySelectorAll('[data-bind]')
-        ),
-        onChanged = this.attributeChangedCallback || noOp
+        )
       ;
       textNodes.forEach(function (node) {
         var
@@ -83,38 +95,55 @@ var Bindings = (function () {'usew strict';
           var
             pair = info.split(colon),
             key = pair[0],
-            value = pair[1] || key
+            value = pair[1] || key,
+            oneWay = value in values,
+            native = key in el,
+            update = true,
+            handler,
+            setter
           ;
-          if (key in el) {
+          if (oneWay) setter = gOPD(values, value).set;
+          if (native) {
             el[key] = bindings[value] || '';
-            dP(values, value, {
-              get: function () { return el[key]; },
-              set: function (value) { el[key] = value; }
-            });
+            dP(values, value, createGetSet(
+              function get() { return el[key]; },
+              function set(value) {
+                if (update) el[key] = value;
+                if (oneWay) setter(el[key]);
+              }
+            ));
+            handler = function (e) {
+              update = false;
+              values[value] = el[key];
+              update = true;
+            };
+            switch (key) {
+              case 'value':
+                el.addEventListener('input', handler, true);
+              case 'checked':
+                el.addEventListener('change', handler, true);
+                break;
+            }
           } else {
+            map[key] = value;
             el.setAttribute(key, bindings[value]);
-            dP(values, value, {
-              get: function () { return el.getAttribute(key); },
-              set: function (value) { el.setAttribute(key, value); }
-            });
+            dP(values, value, createGetSet(
+              function get() { return el.getAttribute(key); },
+              function set(value) {
+                if (update) el.setAttribute(key, value);
+                if (oneWay) setter(el.getAttribute(key));
+              }
+            ));
+            handler = function (e) {
+              update = false;
+              values[value] = el.getAttribute(key);
+              update = true;
+            };
+            if (noMO) el.addEventListener('DOMAttrModified', handler, true);
+            else (new MO(handler)).observe(el, {attributes: true});
           }
         });
       });
-      if (attributes.length) {
-        dP(this, 'onChanged', {
-          configurable: true,
-          value: function (key, p, c) {
-            if (key in values) {
-              if (c == null) {
-                delete values[key];
-              } else {
-                values[key] = c;
-              }
-            }
-            onChanged.call(this, key, p, c);
-          }
-        });
-      }
       return dP(this, 'bindings', {
         configurable: true,
         enumerable: true,
@@ -138,7 +167,7 @@ setTimeout(function () {
   document.body.bindings.name = 'asd';
   document.body.bindings.checked = true;
 }, 500);
-*/
+//*/
 
 /*
 Bindings.withBindings.call(document.body, {
@@ -147,4 +176,34 @@ Bindings.withBindings.call(document.body, {
 });
 
 document.body.bindings.b = 'hello there';
-*/
+//*/
+
+/*
+
+var BoundContent = new DOMClass({
+  with: Bindings
+});
+
+var bc = (new BoundContent).withBindings({
+  template: '<span>{{text}}</span>',
+  bindings: {text: 'Hello World'}
+});
+
+//*/
+
+/*
+
+var EditableNameTag = DOMClass({
+  with: Bindings,
+  name: 'editable-name-tag'
+});
+
+document.body.appendChild(new EditableNameTag).withBindings({
+  template: '<p data-bind="custom:owner">'+
+              'This is a <strong>{{owner}}</strong>\'s editable-name-tag.' +
+            '</p>' +
+            '<input data-bind="value:owner" placeholder="Your name here...">',
+  bindings: {owner: "Daniel"}
+});
+
+//*/
